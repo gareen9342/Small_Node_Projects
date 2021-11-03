2ac785563e700a69ef9df3a3357ee82f94c5b421
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-11-03 22:09:57 +0900

    docs download excel

--- downloadExcel/server/app.js
+++ downloadExcel/server/app.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 })
 
 app.post("/xlsx", (req, res) => {
-
+//https://www.zerocho.com/category/NodeJS/post/60778f07cf47fe0004727b12
     res
         .header("content-disposition","asdf.xlsx").download("./output.xlsx")
 })
